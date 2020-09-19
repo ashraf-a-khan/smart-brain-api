63faade5f241fbe74cc06d90250ed5b743e907dd
ashraf-a-khan <khan.ashraf2311@gmail.com>
2020-09-19 15:00:09 -0400

    changing response

--- server.js
+++ server.js
@@ -51,9 +51,7 @@ const database = {
 	}]
 }
 
-app.get('/', (req, res) => {
-	res.send(database.users);
-})
+app.get('/', (req, res) => {res.send('it is working')})
 
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt)})
 app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
